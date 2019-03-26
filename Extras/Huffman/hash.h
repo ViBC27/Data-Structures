b9054b23b160b0af0433f587e7da2dcd5568225d
Vitor Barcelos <40600648+ViBC27@users.noreply.github.com>
2019-03-26 15:00:59 -0300

    Create hash.h

---
+++ Extras/Huffman/hash.h
@@ -0,0 +1,21 @@
+/* ->
+// Data Structure: Hash
+// Authors: 
+-> Vitor Barcelos de Cerqueira
+-> Ramon Basto Callado
+-> Daniel melo de lima
+-> Erick Pernambuco
+<- */
+
+#ifndef Hash_H
+#define Hash_H
+
+typedef struct hash hash_t;
+struct hash {
+  huff_t **huffTable;
+};
+
+hash_t* new_Hash();
+void erase_Hash(hash_t *hash);
+void put_Hash(hash_t *hash, unsigned char byte);
+void show_Hash(hash_t *hash);
