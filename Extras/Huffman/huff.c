d21cbe822b507737e5efe43a951c7d0b3dfc24e1
Vitor Barcelos <40600648+ViBC27@users.noreply.github.com>
2019-03-26 17:04:07 -0300

    Update huff.c

--- Extras/Huffman/huff.c
+++ Extras/Huffman/huff.c
@@ -9,7 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "huff.h"
+#include <huff.h>
 
 huff_t *newNode_Huff(unsigned char byte) {
   huff_t *newNode = (huff_t*) malloc(sizeof(huff_t));
@@ -20,7 +20,7 @@ huff_t *newNode_Huff(unsigned char byte) {
   return newNode;
 }
 
-huff_t *agroupNodes_Huff(huff_t *left, huff_t *right) {
+huff_t *agroupItems_Huff(huff_t *left, huff_t *right) {
   huff_t *newNode = (huff_t*) malloc(sizeof(huff_t));
   newNode->frequency = left->frequency + right->frequency;
   newNode->left = left;
