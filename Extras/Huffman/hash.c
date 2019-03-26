bff86d568bca8d0d7257a79f77178889ab78aa04
Vitor Barcelos <40600648+ViBC27@users.noreply.github.com>
2019-03-26 15:03:02 -0300

    Update hash.c

--- Extras/Huffman/hash.c
+++ Extras/Huffman/hash.c
@@ -1,39 +1,9 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "hash.h"
+#include "huff.h"
 #define MAX_SIZE 256
 
-typedef struct hash hash_t;
-typedef struct huff huff_t;
-
-struct huff {
-  unsigned char byte;
-  long int frequency;
-  huff_t *left;
-  huff_t *right;
-};
-
-struct hash {
-  huff_t **huffTable;
-};
-
-// Hash
-hash_t* new_Hash();
-void erase_Hash(hash_t *hash);
-void put_Hash(hash_t *hash, unsigned char byte);
-void show_Hash(hash_t *hash);
-
-int main() {
-  hash_t *H1 = new_Hash();
-  put_Hash(H1, 'a');
-  put_Hash(H1, 'b');
-  put_Hash(H1, 'c');
-  show_Hash(H1);
-  erase_Hash(H1);
-  free(H1);
-  return 0;
-}
-
-// Hash
 hash_t* new_Hash() {
   hash_t *newHash = (hash_t*) malloc(sizeof(hash_t));
   newHash->huffTable = (huff_t**) malloc(MAX_SIZE * sizeof(huff_t));
