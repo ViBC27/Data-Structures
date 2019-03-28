5b73bde7be40d942240a0a6e54b0158dfbabcea7
Vitor Barcelos <40600648+ViBC27@users.noreply.github.com>
2019-03-28 11:19:40 -0300

    Update initial.c

--- Extras/Huffman/initial.c
+++ Extras/Huffman/initial.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void encode() {
-  printf("Encode.\n");
+void compress(char *pathFile, char *outputFile) {
+  printf("Compress.\n");
 }
 
-void decode() {
-  printf("Decode.\n");
+void descompress(char *pathFile, char *outputFile) {
+  printf("Descompress.\n");
 }
 
 void help() {
@@ -15,8 +15,8 @@ void help() {
   
   printf("\n");
   printf("APPLICATION OPTIONS:\n");
-  printf("-e, --encode\t\tEncode a file.\n");
-  printf("-d, --decode\t\tDecode a file.\n");
+  printf("-c, --compress\t\tcompress a file.\n");
+  printf("-d, --descompress\t\tdescompress a file.\n");
   printf("-v, --version\t\tShows the version of the application.\n");
   
   printf("\n");
@@ -33,30 +33,37 @@ void version() {
   printf("VERD v.ALPHA-0.1\n");
 }
 
-int stringEquality(char *S1, char *S2) {
-  return strcmp(S1, S2);
-}
-
 int main(int argc, char *argv[]) {
+  char *command = argv[1];
+  char outputFile[100];
+  char pathFile[100];
 
   if(argc == 1) {
-    printf("Uso principal.\n");
+    printf("Welcome to VRDZIP.\n");
+    printf("Write the path of the file: ");
+    scanf("%s", pathFile);
   }
   else if(argc == 2) {
-    if(stringEquality(argv[1], "-h") == 0) help();
-    else if(stringEquality(argv[1], "--help") == 0) help();
-    else if(stringEquality(argv[1], "-v") == 0) version();
-    else if(stringEquality(argv[1], "--version") == 0) version();
-    else if(stringEquality(argv[1], "-e") == 0) encode();
-    else if(stringEquality(argv[1], "--encode") == 0) encode();
-    else if(stringEquality(argv[1], "-d") == 0) decode();
-    else if(stringEquality(argv[1], "--decode") == 0) decode();
+    if(strcmp(command, "-h") == 0) help();
+    else if(strcmp(command, "--help") == 0) help();
+    else if(strcmp(command, "-v") == 0) version();
+    else if(strcmp(command, "--version") == 0) version();
     else {
-      printf("%s: Unknown option.\n", argv[1]);
+      printf("%s: Unknown option.\n", command);
     }
-
   }
-  else if(argc == 3) {}
-  else {}
+  else if(argc == 4) {
+    //pathFile = argv[2]; outputFile = argv[3];
+    if(strcmp(command, "-c") == 0) compress(pathFile, outputFile);
+    else if(strcmp(command, "--compress") == 0) compress(pathFile, outputFile);
+    else if(strcmp(command, "-d") == 0) descompress(pathFile, outputFile);
+    else if(strcmp(command, "--descompress") == 0) descompress(pathFile, outputFile);
+    else {
+      printf("%s: Unknown option.\n", command);
+    }
+  }
+  else {
+    printf("Unknown options.\n");
+  }
   return 0;
 }
