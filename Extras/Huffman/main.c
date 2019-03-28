050a69c6f0cc6fb6975b45dcd32d49b0fa7d578a
Vitor Barcelos <40600648+ViBC27@users.noreply.github.com>
2019-03-28 10:39:58 -0300

    Update main.c

--- Extras/Huffman/main.c
+++ Extras/Huffman/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <hash.h>
 #include <heap.h>
+
+huff_t* MountHuffTree(heap_t *heap) {
+  huff_t *item1, *item2, *item3;
+  while(heap->size > 1) {
+    item1 = dequeue(heap);
+    item2 = dequeue(heap);
+    item3 = agroupItems_Huff(item1, item2);
+    enqueue(heap, item3);
+  }
+  return dequeue(heap);
+}
 
 void HashToHeap(hash_t * hash, heap_t *heap) {
   int i;
@@ -11,42 +24,43 @@ void HashToHeap(hash_t * hash, heap_t *heap) {
   }
 }
 
-void MountHuffTree(heap_t *heap) {
-  huff_t *item1, *item2, *item3;
-  while(heap->size > 1) {
-    item1 = dequeue(heap);
-    item2 = dequeue(heap);
-    item3 = agroupItems_Huff(item1, item2);
-    enqueue(heap, item3);
+void compress(FILE *pathFile) {
+  heap_t *heap = new_Heap();
+  hash_t *hash = new_Hash();
+  unsigned char byte;
+  while(fscanf(pathFile, "%c", &byte) != EOF) {
+    put_Hash(hash, byte);
   }
-  
-  printf("Aqui\n");
+  HashToHeap(hash, heap);
+  huff_t *root = MountHuffTree(heap);
+  showHuffman(root);
+
 }
 
-/*
-void showHuffman() {
-  if(!isEmptyAux_BSTree(bt)) {
-    printf("(%d", bt->value);
-    showPreOrderAux_BSTree(bt->left);
-    showPreOrderAux_BSTree(bt->right);
-    printf(")");
+void readFile(int file) {
+  char path[100];
+  scanf(" %s", path);
+  FILE *pathFile;
+  pathFile = fopen (path,"rb");
+  if(pathFile == NULL) {
+    printf("Not open file.\n");
+    exit(27);
   }
   else {
-    printf("()");
+    if(file == 1) {
+      compress(pathFile);
+    }
+    else {
+      printf("Descompress\n");
+      strcat(path, ".huff");
+    }
+    fclose (pathFile);
   }
 }
-*/
 
 int main() {
-  heap_t *HP = new_Heap();
-  hash_t *H1 = new_Hash();
-  put_Hash(H1, 'a');
-  put_Hash(H1, 'a');
-  put_Hash(H1, 'a');
-  put_Hash(H1, 'b');
-  put_Hash(H1, 'b');
-  put_Hash(H1, 'c');
-  HashToHeap(H1, HP);
-  MountHuffTree(HP);
+  int file;
+  scanf("%d", &file);
+  readFile(file);
   return 0;
 }
