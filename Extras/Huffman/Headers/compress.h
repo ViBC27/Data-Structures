f4dbbdcb9660140661c6801a52d0229dcc9ca959
ViBC27 <Vbc@ic.ufal.br>
2019-04-03 00:29:11 -0300

    Recomeco

--- Extras/Huffman/Headers/compress.h
+++ Extras/Huffman/Headers/compress.h
@@ -15,10 +15,10 @@
 void compress(char *inputFile, char *outputFile);
 void ReadInputFile(FILE *inFile, hash_t *hash);
 void HashToHeap(hash_t * hash, heap_t *heap);
-huff_t* MountHuffTree_Comp(heap_t *heap, huff_t **root);
+huff_t* MountHuffTree_Comp(heap_t *heap);
 void MountInfoTable(huff_t *tree, info_t *bInfo, bool bits[], int totalBits);
-void MountHeader(huff_t *tree, char *outputFile, int treeSize, int trashSize, byte_t *oneByte);
-void MountFile_Comp(huff_t *tree, info_t *bInfo, char *inputFile, char *outputFile, byte_t oneByte);
+byte_t* MountHeader(int treeSize, int trashSize);
+void MountFile_Comp(huff_t *tree, info_t *bInfo, char *inputFile, char *outputFile, byte_t *header);
 unsigned int setBit(unsigned int byte, int i);
 int size_Trash(info_t *bInfo);
 
