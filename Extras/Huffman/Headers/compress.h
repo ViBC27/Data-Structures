b77b749c3f530f90493f21c3c7f2212ad9ed426e
Vitor Barcelos <40600648+ViBC27@users.noreply.github.com>
2019-04-02 22:10:17 -0300

    Update compress.h

--- Extras/Huffman/Headers/compress.h
+++ Extras/Huffman/Headers/compress.h
@@ -15,10 +15,10 @@
 void compress(char *inputFile, char *outputFile);
 void ReadInputFile(FILE *inFile, hash_t *hash);
 void HashToHeap(hash_t * hash, heap_t *heap);
-huff_t* MountHuffTree_Comp(heap_t *heap);
+huff_t* MountHuffTree_Comp(heap_t *heap, huff_t **root);
 void MountInfoTable(huff_t *tree, info_t *bInfo, bool bits[], int totalBits);
-byte_t* MountHeader(int treeSize, int trashSize);
-void MountFile_Comp(huff_t *tree, info_t *bInfo, char *inputFile, char *outputFile, byte_t *header);
+void MountHeader(huff_t *tree, char *outputFile, int treeSize, int trashSize, byte_t *oneByte);
+void MountFile_Comp(huff_t *tree, info_t *bInfo, char *inputFile, char *outputFile, byte_t oneByte);
 unsigned int setBit(unsigned int byte, int i);
 int size_Trash(info_t *bInfo);
 
